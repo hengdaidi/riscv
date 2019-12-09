55f7bba58b7852cde58cde4955b0dc338dc69616
bluew <bluewww@users.noreply.github.com>
2019-12-09 05:33:40 +0100

    TB: Add atomics shim infront on RAM model

--- tb/core/riscv_wrapper.sv
+++ tb/core/riscv_wrapper.sv
@@ -40,6 +40,7 @@ module riscv_wrapper
     logic [3:0]                   data_be;
     logic [31:0]                  data_rdata;
     logic [31:0]                  data_wdata;
+    logic [5:0]                   data_atop;
 
     // signals to debug unit
     logic                         debug_req_i;
@@ -88,6 +89,7 @@ module riscv_wrapper
          .data_rdata_i           ( data_rdata            ),
          .data_gnt_i             ( data_gnt              ),
          .data_rvalid_i          ( data_rvalid           ),
+         .data_atop_o            ( data_atop             ),
 
          .apu_master_req_o       (                       ),
          .apu_master_ready_o     (                       ),
@@ -138,6 +140,7 @@ module riscv_wrapper
          .data_rdata_o   ( data_rdata                     ),
          .data_rvalid_o  ( data_rvalid                    ),
          .data_gnt_o     ( data_gnt                       ),
+         .data_atop_i    ( data_atop                      ),
 
          .irq_id_i       ( irq_id_out                     ),
          .irq_ack_i      ( irq_ack                        ),
